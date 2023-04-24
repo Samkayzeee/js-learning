e1cb342387cb65e5744e440d609729f293ff95b5
Samkayzeee <lasisiabdulsamad9@gmail.com>
2023-04-24 16:52:41 +0100

    added function for roman numerals to intergers

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ const FizzBuzz = (n) => {
 }
 
 // FizzBuzz(15);
+
+
+let Romans = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+};
+
+const romanToNumber  = (roman) => {
+    let value  = 0;
+
+    for (let i = value; i < roman.length; i++) {
+        Romans[roman[i]] < Romans[roman[i + 1]] ? (value -= Romans[roman[i]]) : (value += Romans[roman[i]])
+    }
+
+    return value;
+};
+
+// console.log(romanToNumber('MX'));
